0db4c0ebf0851d5c3637ac68468e22f925a19c70
sumit1553 <sumit.n1553@gmail.com>
2025-08-28 16:27:50 +0530

    fifth commit

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use server';
 
 // import { isRedirectError } from 'next/dist/client/components/redirect';
@@ -12,12 +13,12 @@ import { getMyCart } from './cart.actions';
 import { getUserById } from './user.actions';
 import { insertOrderSchema } from '../validators';
 import { prisma } from '@/db/prisma';
-import { CartItem, PaymentResult, ShippingAddress } from '@/types';
+import { CartItem, PaymentResult } from '@/types';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
 import { PAGE_SIZE } from '../constants';
 import { Prisma } from '@prisma/client';
-import { sendPurchaseReceipt } from '@/email';
+// import { sendPurchaseReceipt } from '@/email';
 
 
 
